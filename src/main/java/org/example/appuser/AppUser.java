d7f0e440b0b92e5e1ab73100bbbf9ea1e1f7ae86
Reyan Shariff <rts34@duke.edu>
2025-12-31 16:41:19 -0500

    Update pom, add AppUser class

---
+++ src/main/java/org/example/appuser/AppUser.java
@@ -0,0 +1,22 @@
+package org.example.appuser;
+import java.util.Collection;
+import java.util.List;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+public class AppUser implements UserDetails {
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of();
+  }
+
+  @Override
+  public String getPassword() {
+    return "";
+  }
+
+  @Override
+  public String getUsername() {
+    return "";
+  }
+}
